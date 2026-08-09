380b5b7aafb00fb6e9d439ff93b67eab914b1c0e
kimsj0970 <kimsj0970@gmail.com>
2026-08-09 17:28:29 +0900

    [FEAT] UserJpaRepository에 findAllByIdIn 쿼리 메서드 추가

--- src/main/java/mukkeu/mukkeu/user/infrastructure/persistence/UserJpaRepository.java
+++ src/main/java/mukkeu/mukkeu/user/infrastructure/persistence/UserJpaRepository.java
@@ -1,5 +1,6 @@
 package mukkeu.mukkeu.user.infrastructure.persistence;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import mukkeu.mukkeu.user.domain.User;
 
 public interface UserJpaRepository extends JpaRepository<User, Long> {
+
+	List<User> findAllByIdIn(List<Long> ids);
 
 	Optional<User> findByEmail(String email);
 
